db76cd89ffbcc1c52df793962dbb89e7ae87fba2
Jeanne2 <72031513+Jeanne2@users.noreply.github.com>
2021-06-16 15:33:50 -0300

    Add files via upload
    Objetivo
    ## Acessar página do facebook.com;
    ## Preencher os campos de cadastro.

---
+++ FaceTeste.java
@@ -0,0 +1,58 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FaceTeste {
+    WebDriver driver;
+
+    @Before
+    public void antes(){
+      // Configura Selenium
+        System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://pt-br.facebook.com/r.php");
+    }
+
+    @Test
+    public void facebookTesteLogin() throws InterruptedException {
+
+        // Preenhe Campos de cadastro
+        driver.findElement(By.name("firstname")).sendKeys("Jeanne");
+        driver.findElement(By.name("lastname")).sendKeys("Barbosa");
+        driver.findElement(By.name("reg_email__")).sendKeys("jeannebarbosa@gmail.com");
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("jeannebarbosa@gmail.com");
+        driver.findElement(By.name("reg_passwd__")).sendKeys("123asd");
+
+        // Preenche os select de data
+        WebElement comboDia = driver.findElement(By.id("day"));
+        Select select = new Select(comboDia);
+        select.selectByVisibleText("26");
+
+        WebElement comboMes = driver.findElement(By.id("month"));
+        select = new Select(comboMes);
+        select.selectByVisibleText("Mar");
+
+        WebElement comboAno = driver.findElement(By.id("year"));
+        select = new Select(comboAno);
+        select.selectByVisibleText("1985");
+
+        // Escolha do gênero
+        driver.findElement(By.xpath("//input[@value='1']")).click();
+
+        // Clica no botão Entrar
+        driver.findElement(By.name("websubmit")).click();
+    }
+
+   // @After
+   // public void depois(){
+    //    driver.quit();
+
+    }
+
